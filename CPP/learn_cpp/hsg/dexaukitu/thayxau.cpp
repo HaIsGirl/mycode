fc95f0c934d9fc4cf9c4cad7033f3379266f949a
HaThai088 <doanha03102008>
2023-10-08 23:12:24 +0700

    update

--- CPP/learn_cpp/hsg/dexaukitu/thayxau.cpp
+++ CPP/learn_cpp/hsg/dexaukitu/thayxau.cpp
@@ -7,5 +7,13 @@ int main(){
     getline(cin,s1);
     getline(cin,s2);
     getline(cin,s3);
-    
+    int i=0;
+    while (i<s3.size()){
+        if (s3.substr(i,s1.size()) == s1){
+            s3.erase(i,s1.size());
+            s3.insert(i,s2);
+        }
+        i++;
+    }
+    cout<<s3<<endl;
 }

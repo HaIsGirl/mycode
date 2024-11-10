eba7be0524c9bef7393eff13266bdcda30f6ae8e
HaIsGirl <hthathai0310@gmail.com>
2024-11-10 23:07:11 +0700

    ...

--- cayde/Code.cpp
+++ cayde/Code.cpp
@@ -1,20 +1,60 @@
-#include<iostream>
-#include<unordered_map>
+#include<bits/stdc++.h>
 using namespace std;
+
+pair<int,int> kanade(vector<int> A){
+    int sum_curent = 0 ;
+    int sum_max = 0;
+    int l = 0,r = 0;
+    for (int i = 0; i<A.size();i++){
+        sum_curent +=A[i];
+        if (sum_curent < 0){
+            sum_curent = 0;
+            l = i+1;
+        }
+        if(sum_max <= sum_curent){
+            sum_max = sum_curent;
+            r=i;
+        }
+    }
+    return {l,r};
+}
+
+void solve(int n, string s){
+    vector<int> A;
+    int binary_sum = 0; 
+    for (int i = 0; i<n;i++){
+        if (s[i] == '1'){
+            A.push_back(-1);
+            binary_sum++;
+        }
+        else
+            A.push_back(1);
+    }
+    if (binary_sum == n && binary_sum == 0){
+        cout<<n<<endl;
+        return;
+    }
+    auto pr = kanade(A);
+    //cout<<pr.first<<" "<<pr.second<<endl;
+    //cout<<binary_sum<<endl;
+    for (int i = pr.first; i<=pr.second;i++){
+        if (s[i] == '0')
+            binary_sum++;
+        else
+            binary_sum--;
+        cout<<s[i]<<" ";
+    }
+    cout<<endl;
+    cout<<binary_sum<<endl;
+
+}
+
+
 int main(){
-    unordered_map<int,int> mp;
     int n;
     cin>>n;
-    int A[n+5];
-    for (int i = 1; i<=n;i++){
-        cin>>A[i];
-    }
-    int rs = 0;
-    for (int i = 1; i<=n;i++){
-        if (mp.find(A[i]) != mp.end()){
-            rs+=mp[A[i]];
-        }
-        mp[A[i]]++;
-    }
-    cout<<rs<<endl;
+    string s;
+    cin>>s;
+    n = s.size();
+    solve(n,s);
 }
--- cayde/docao.cpp
+++ cayde/docao.cpp
@@ -3,31 +3,40 @@ using namespace std;
 const int Nmax = 1 + 1e6;
 bool A[Nmax] = {false};
 int main(){
-    int n,h;
-    cin>>n>>h;
-    int s;
-    int d = 0;
-    for (int i=2; i*i<=n; i++){
-        for (int j = 2; j*i <= n; j++){
-            if (A[i] == 0){
-                A[i*j] = 1;
-            }
-        }
-    }
+    // int n,h;
+    // cin>>n>>h;
+    // int s;
+    // int d = 0;
+    // for (int i=2; i*i<=n; i++){
+    //     for (int j = 2; j*i <= n; j++){
+    //         if (A[i] == 0){
+    //             A[i*j] = 1;
+    //         }
+    //     }
+    // }
 
-    for (int i = 2;i<=n;i++){
-        s = 0;
-        if (A[i] == 0){
-            int temp = i;
-            while (temp > 0){
-                s += temp % 10;
-                temp = temp /10;
-            }
-            if (s == h){
-                cout<<i<<endl;
-                d++;
-            }
+    // for (int i = 2;i<=n;i++){
+    //     s = 0;
+    //     if (A[i] == 0){
+    //         int temp = i;
+    //         while (temp > 0){
+    //             s += temp % 10;
+    //             temp = temp /10;
+    //         }
+    //         if (s == h){
+    //             cout<<i<<endl;
+    //             d++;
+    //         }
+    //     }
+    // }
+    // cout<<d<<endl;
+    int cnt = 0;
+    for (int i = 1; i<=5; i++){
+        for (int j = i; j<=5; j++){
+            if (1 + 1 == 2)
+                cnt++;
         }
     }
-    cout<<d<<endl;
+    cout<<cnt;
 }
+//1 1 1 1 1
